3825f64cae54736db23035fbb146130626b7b878
raqlol <raquel.lowell@gmail.com>
2017-05-02 14:21:16 -0700

    updated function to trigger aftr scrolling beings

--- js/site.js
+++ js/site.js
@@ -77,9 +77,9 @@ function slide() {
 }
 
 // float up
+window.onscroll = function() {float()};
 function float() {
     if (document.body.scrollTop > 730 || document.documentElement.scrollTop > 730) {
         document.getElementById("float").className = "floatToTop";
     }
 }
-float()
