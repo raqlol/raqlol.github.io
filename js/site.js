15bfa83ad0cca6dfda30b9dadd5bff4ee3aa5220
raqlol <raquel.lowell@gmail.com>
2018-05-03 17:08:31 -0700

    tired and taking forever to fix this slider

--- js/site.js
+++ js/site.js
@@ -21,8 +21,9 @@ function float() {
 }
 
 const fadeSlider = document.getElementsByClassName("slider")
+
 (function () {
-  for (var i=0; i <slider.length; i++) {
+  for (var i=0; i <fadeSlider.length; i++) {
     var next = i + 1;
     fadeSlider[i].classList.add("fade");
     fadeSlider[next].classList.remove("fade");
