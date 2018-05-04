cf7aae13ba6dca143ba41544d85bfd9824f0b0b3
raqlol <raquel.lowell@gmail.com>
2018-05-03 17:11:23 -0700

    tired and taking forever to fix this slider

--- js/site.js
+++ js/site.js
@@ -1,26 +1,4 @@
-// menu
-const menuIcon = document.getElementById("menu-icon");
-const dropdown = document.getElementById("dropdown");
-const triangle = document.getElementById("triangle-menu");
-
-function determinesState () {
-  this.classList.toggle('open');
-  var opened = dropdown.classList.contains('open-menu');
-  if (opened) {
-    dropdown.classList.remove('open-menu');
-    triangle.classList.remove('open-menu');
-  }
-  else {
-    dropdown.classList.add('open-menu');
-    triangle.classList.add('open-menu');
-  }
-}
-window.onscroll = function() {float()};
-function float() {
-  document.getElementsByClassName("float").className = "floatToTop";
-}
-
-const fadeSlider = document.getElementsByClassName("slider")
+const fadeSlider = document.getElementsByClassName("slider");
 
 (function () {
   for (var i=0; i <fadeSlider.length; i++) {
