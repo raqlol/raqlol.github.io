12dbb5a7ff04c287c7a1d7df08f51807fd37c7d0
clowell <clayton.r.lowell@gmail.com>
2018-09-02 15:20:19 -0700

    changed increment

--- kong/scripts/main.js
+++ kong/scripts/main.js
@@ -7,9 +7,28 @@ function counter(element) {
   console.log(endNum)
   console.log(element)
   while (displayNum < endNum) {
-    element.innerHTML = displayNum;
-    displayNum += increment
-    console.log(displayNum)
+
+    if (displayNum < 9) {
+      increment = 1;
+      element.innerHTML = displayNum;
+      displayNum += increment
+    }
+    else if(displayNum > 99) {
+      increment = 10;
+      element.innerHTML = displayNum;
+      displayNum += increment
+    }
+    else if(displayNum > 999) {
+      increment = 100;
+      element.innerHTML = displayNum;
+      displayNum += increment
+    }
+    else if(displayNum > 9999) {
+      increment = 1000;
+      displayCondensed = displayNum/1000
+      element.innerHTML = "+" + displayCondensed + "K";
+      displayNum += increment
+    }
   }
   console.log("done")
 }
