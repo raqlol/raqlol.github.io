d9ffa448750d5ba4ef0f3d2e858acc771462172f
clowell <clayton.r.lowell@gmail.com>
2018-09-02 15:31:54 -0700

    trying stuff

--- kong/scripts/main.js
+++ kong/scripts/main.js
@@ -1,7 +1,6 @@
 function counter(element) {
   let displayNum = 0;
   let endNum = parseInt(element.getAttribute("data-number"));
-  let counter = element;
   let increment = 1;
   console.log(displayNum)
   console.log(endNum)
@@ -10,25 +9,24 @@ function counter(element) {
 
     if (displayNum < 9) {
       increment = 1;
-      setTimeout(function(){ element.innerHTML = displayNum;}, 10);
       displayNum += increment
+      setTimeout(function(){ element.innerHTML = displayNum;}, 10);
     }
     else if(displayNum > 99) {
       increment = 10;
-      setTimeout(function(){ element.innerHTML = displayNum;}, 100);
       displayNum += increment
+      setTimeout(function(){ element.innerHTML = displayNum;}, 100);
     }
     else if(displayNum > 999) {
       increment = 100;
-      setTimeout(function(){element.innerHTML = displayNum;}, 500);
       displayNum += increment
+      setTimeout(function(){element.innerHTML = displayNum;}, 500);
     }
     else if(displayNum > 9999) {
       increment = 1000;
       displayCondensed = displayNum/1000
-      setTimeout(function(){ element.innerHTML = "+" + displayCondensed + "K";}, 100);
-
       displayNum += increment
+      setTimeout(function(){ element.innerHTML = "+" + displayCondensed + "K";}, 100);
     }
     console.log(displayNum)
   }
