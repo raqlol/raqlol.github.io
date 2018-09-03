f40af6cf415b20a7a657c6a7f01d5f8294aa1aa0
clowell <clayton.r.lowell@gmail.com>
2018-09-03 15:08:31 -0700

    speeding up counter

--- kong/scripts/main.js
+++ kong/scripts/main.js
@@ -6,8 +6,25 @@ const metricThree = document.getElementById('metric-three')
 function visibleCounter (element) {
 	let endNum = parseInt(element.getAttribute("data-number"));
   let increment = 1;
-  setInterval(function(){ if (increment < 100) {element.innerHTML = increment; increment++}}, 100)
-  setInterval(function(){  if (increment >= 100 && increment < endNum) {element.innerHTML = increment; increment++}}, 1000)
+	let displayNum;
+  setInterval(function(){
+
+		if (increment < 1000) {
+			element.innerHTML = increment;
+			increment++;
+		}
+		else if (increment <= 999999 && increment >= 1000) {
+			displayNum = "+"(increment/1000).toFixed(0) + "K"
+			element.innerHTML = displayNum;
+			increment++;
+		}
+		else if (increment >= 999999) {
+			displayNum = "+"(increment/1000000).toFixed(0) + "M"
+			element.innerHTML = displayNum;
+			increment++;
+		}
+	}, 50)
+  //setInterval(function(){  if (increment >= 100 && increment < endNum) {element.innerHTML = increment; increment++}}, 1000)
 }
 visibleCounter(metricOne)
 visibleCounter(metricTwo)
