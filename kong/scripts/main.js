34d9d440a3ce876377f7cdb33d2601242d5bfad8
clowell <clayton.r.lowell@gmail.com>
2018-09-03 15:01:57 -0700

    basic counter

--- kong/scripts/main.js
+++ kong/scripts/main.js
@@ -1,12 +1,14 @@
 
-const test = document.getElementById('test')
-const tester = document.getElementById('tester')
+const metricOne = document.getElementById('metric-one')
+const metricTwo = document.getElementById('metric-two')
+const metricThree = document.getElementById('metric-three')
 
-let startNum = 0;
-function testingCount () {
-	let endNum = parseInt(test.getAttribute("data-number"));
+function visibleCounter (element) {
+	let endNum = parseInt(element.getAttribute("data-number"));
   let increment = 1;
-  setInterval(function(){ if (increment < 100) {test.innerHTML = increment; increment++}}, 100)
-  setInterval(function(){  if (increment >= 100 && increment < 120) {test.innerHTML = increment; increment++}}, 1000)
+  setInterval(function(){ if (increment < 100) {element.innerHTML = increment; increment++}}, 100)
+  setInterval(function(){  if (increment >= 100 && increment < endNum) {element.innerHTML = increment; increment++}}, 1000)
 }
-testingCount()
+visibleCounter(metricOne)
+visibleCounter(metricTwo)
+visibleCounter(metricThree)
