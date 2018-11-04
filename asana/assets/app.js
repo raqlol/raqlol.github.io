0b5de8bacd633da8706326715e27365dfdc45446
raqlol <=>
2018-11-04 09:58:42 -0800

    close button for popup

--- asana/assets/app.js
+++ asana/assets/app.js
@@ -1,9 +1,23 @@
 // resize header on scroll
 function shrinkMasthead() {
-  const masthead = document.getElementById("masthead")
-  console.log(masthead)
-  masthead.classList.add("shrink")
+  let masthead = document.getElementById("masthead")
+  let scroll = window.scrollTop;
+        if (scroll >= 600) {
+              masthead.classList.add("shrink")
+        } else {
+              masthead.classList.remove("shrink")
+        }
 }
+
 window.onload = function () {
+  const closeBtns = document.getElementsByClassName("close")
+  /* event listener for shrinking masthead */
   window.addEventListener("scroll", shrinkMasthead);
+  /* event listener to close popups */
+  for (i=0;i<closeBtns.length;i++) {
+    closeBtns[i].addEventListener("click", function(event){
+      let dogThumbnail = event.target
+      dogThumbnail.parentNode.classList.add("dog-thumbnail");
+    })
+  }
 }
