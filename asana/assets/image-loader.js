f035fc16822715001e6f426c39d17048b497b65b
raqlol <=>
2018-11-04 19:40:44 -0800

    cleaning up bio creator

--- asana/assets/image-loader.js
+++ asana/assets/image-loader.js
@@ -4,6 +4,7 @@ const lazyClass = document.getElementsByClassName('lazy');
 const dogPic = document.getElementsByClassName("pic");
 const dogThumbnail = document.getElementsByClassName("dog-thumbnail");
 const closeBtns = document.getElementsByClassName("close");
+const dogGallery = document.getElementById("dog-gallery");
 // image expander
 function expandBio(e, state) {
   if(state == true) {
@@ -26,87 +27,103 @@ function createImg(array, i){
   imgContainer.classList.add("img-container")
   return imgContainer
 }
+function createName(array, i){
+  let name = document.createElement("p");
+  let nameTxt = document.createTextNode(array[i].name);
+  name.appendChild(nameTxt)
+  name.classList.add("name")
+  return name
+}
+function createAge(array, i){
+  let age = document.createElement("p");
+  let ageTxt = document.createTextNode(array[i].age);
+  age.appendChild(ageTxt)
+  age.classList.add("age")
+  return age
+}
+function createSize(array, i){
+  let size = document.createElement("p");
+  let sizeTxt = document.createTextNode(array[i].size);
+  size.appendChild(sizeTxt)
+  size.classList.add("size")
+  return size
+}
+function createGender(array, i){
+  let gender = document.createElement("p");
+  let genderTxt = document.createTextNode(array[i].gender);
+  gender.appendChild(genderTxt)
+  gender.classList.add("gender")
+  return gender
+}
+function createBreed(array, i){
+  let breed = document.createElement("p");
+  let breedTxt = document.createTextNode(array[i].breed);
+  breed.appendChild(breedTxt)
+  breed.classList.add("breed")
+  return breed
+}
+function createBio(array, i){
+  let bio = document.createElement("p");
+  let bioTxt = document.createTextNode(array[i].bio);
+  bio.appendChild(bioTxt)
+  bio.classList.add("bio")
+  return bio
+}
 /* see above for all created parts */
 function createBio(array) {
-    const dogGallery = document.getElementById("dog-gallery");
     for (i=0;i<array.length;i++){
+      /* create the outter container */
       let container = document.createElement("div");
       container.classList.add("dog-thumbnail");
       container.classList.add("dog");
-
-      let name = document.createElement("p");
-      let nameTxt = document.createTextNode(array[i].name);
-      name.appendChild(nameTxt)
-      name.classList.add("name")
-      let age = document.createElement("p");
-      let ageTxt = document.createTextNode(array[i].age);
-      age.appendChild(ageTxt)
-      age.classList.add("age")
-      let breed = document.createElement("p");
-      let breedTxt = document.createTextNode(array[i].breed);
-      breed.appendChild(breedTxt)
-      breed.classList.add("breed")
-      let gender = document.createElement("p");
-      let genderTxt = document.createTextNode(array[i].gender);
-      gender.appendChild(genderTxt)
-      gender.classList.add("gender")
-      let size = document.createElement("p");
-      let sizeTxt = document.createTextNode(array[i].size);
-      size.appendChild(sizeTxt)
-      size.classList.add("size")
-      let bio = document.createElement("p");
-      let bioTxt = document.createTextNode(array[i].bio);
-      bio.appendChild(bioTxt)
-      bio.classList.add("bio")
+      /* create the buttons on every popup */
       let close = document.createElement("p");
       let closeTxt = document.createTextNode("✖");
-
       let adoptBtn = document.createElement("button");
-
-
       close.appendChild(closeTxt);
       close.classList.add("close")
       close.setAttribute("aria-label", "close")
+      /* putting all the peices together */
       container.appendChild(close)
       container.appendChild(createImg(array, i))
-      container.appendChild(name)
-      container.appendChild(size)
-      container.appendChild(age)
-      container.appendChild(gender)
-      container.appendChild(breed)
-      container.appendChild(bio)
+      container.appendChild(createName(array, i))
+      container.appendChild(createSize(array, i))
+      container.appendChild(createAge(array, i))
+      container.appendChild(createGender(array, i))
+      container.appendChild(createBreed(array, i))
+      container.appendChild(createBio(array, i))
       container.appendChild(adoptBtn)
       dogGallery.appendChild(container)
     }
     /* lazy load */
-  let lazyImages = [].slice.call(document.querySelectorAll("div.lazy"));
-    let active = false;
-    const lazyLoad = function() {
-      if (active === false) {
-        active = true;
-        setTimeout(function() {
-          lazyImages.forEach(function(lazyImage) {
-            if (
-              lazyImage.getBoundingClientRect().top <= window.innerHeight &&
-              lazyImage.getBoundingClientRect().bottom >= 0 &&
-              getComputedStyle(lazyImage).display !== "none"
-            ) {
-              lazyImage.style.backgroundImage = "url('" + lazyImage.dataset.src + "')"
-              lazyImage.classList.remove("lazy");
-              lazyImages = lazyImages.filter(function(image) {
-                return image !== lazyImage;
-              });
-              if (lazyImages.length === 0) {
-                document.removeEventListener("scroll", lazyLoad);
-                window.removeEventListener("resize", lazyLoad);
-                window.removeEventListener("orientationchange", lazyLoad);
+    let lazyImages = [].slice.call(document.querySelectorAll("div.lazy"));
+      let active = false;
+      const lazyLoad = function() {
+        if (active === false) {
+          active = true;
+          setTimeout(function() {
+            lazyImages.forEach(function(lazyImage) {
+              if (
+                lazyImage.getBoundingClientRect().top <= window.innerHeight &&
+                lazyImage.getBoundingClientRect().bottom >= 0 &&
+                getComputedStyle(lazyImage).display !== "none"
+              ) {
+                lazyImage.style.backgroundImage = "url('" + lazyImage.dataset.src + "')"
+                lazyImage.classList.remove("lazy");
+                lazyImages = lazyImages.filter(function(image) {
+                  return image !== lazyImage;
+                });
+                if (lazyImages.length === 0) {
+                  document.removeEventListener("scroll", lazyLoad);
+                  window.removeEventListener("resize", lazyLoad);
+                  window.removeEventListener("orientationchange", lazyLoad);
+                }
               }
-            }
-          });
-          active = false;
-        }, 100);
-      }
-    };
+            });
+            active = false;
+          }, 100);
+        }
+      };
     document.addEventListener("scroll", lazyLoad);
     window.addEventListener("resize", lazyLoad);
     window.addEventListener("orientationchange", lazyLoad);
@@ -127,13 +144,11 @@ function createBio(array) {
     }
 }
 (function fetchJSONFile() {
-    console.log("calling all json")
     var httpRequest = new XMLHttpRequest();
     let xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4) {
             if (xhr.status === 200) {
-              console.log("success")
                 dogBios = (JSON.parse(xhr.responseText)).dogs
                 createBio(dogBios);
             }
@@ -141,11 +156,4 @@ function createBio(array) {
     };
     xhr.open('GET', "https://raqlol.github.io/asana/assets/data/dogs.json", true);
     xhr.send();
-  console.log("done")
 })();
-
-// image filter
-
-// random wiggle
-
-// konami code
