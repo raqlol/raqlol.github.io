0c7182103c55c2c394f5475c418158af1d8e92ab
raqlol <raquel.lowell@gmail.com>
2019-11-29 11:11:59 -0800

    new website idea

--- js/site.js
+++ js/site.js
@@ -7,3 +7,18 @@ const fadeSlider = document.getElementsByClassName("slider");
     fadeSlider[next].classList.remove("fade");
   }
 });
+
+var menuIcon = document.getElementById("menu-icon");
+menuIcon.addEventListener("click", determinesState);
+
+function determinesState () {
+  this.classList.toggle('open');
+  var dropdown = document.getElementById("dropdown")
+  var opened = dropdown.classList.contains('open-menu');
+  if (opened) {
+    dropdown.classList.remove('open-menu');
+  }
+  else {
+    dropdown.classList.add('open-menu');
+  }
+}
